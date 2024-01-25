787bee1c94521411a381da1d4efb54eb6089bfe9
Mohammed <sajidcurious9@gmail.com>
2024-01-25 21:55:07 +0530

    added required functions in the reducer

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -7,5 +7,18 @@ const initialState = {
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
-  reducers: {},
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
 });
+
+export const { addTodo, removeTodo } = todoSlice.actions;
