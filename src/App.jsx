de48dc99b88e33f180d9baa3312d03c4459cd373
Mohammed <sajidcurious9@gmail.com>
2024-01-26 00:02:31 +0530

    completed building simple counter app using the redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./redux/counterSlice";
 
 function App() {
+  const count = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <>
-      <h1>Happy</h1>
+      <h1>Count is {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrement
+      </button>
     </>
   );
 }
