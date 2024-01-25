fda39e2a2391c2da058a8eb8206c2cdadc1a0b2c
Mohammed <sajidcurious9@gmail.com>
2024-01-25 23:45:01 +0530

    deleted components and using app.js to demonstrate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   return (
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     counter: counterSlice,
   },
 });
+
+export default store;
