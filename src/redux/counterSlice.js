1404e7c9e85fcd80ee69ee69a491aff3895f6244
Mohammed <sajidcurious9@gmail.com>
2024-01-25 23:26:18 +0530

    edited reducers

--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -1,9 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const counterSlice = createSlice({
+  name: "counter",
   initialState: 0,
-  name: "todo",
-  reducers: {},
+  reducers: {
+    increment: (state) => state + 1,
+    decrement: (state) => state - 1,
+  },
 });
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
